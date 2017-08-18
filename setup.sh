8a90888595ca4dc6386caba909b02df1a27c7b32
frikky <frikky@dev.wsg.int>
2017-08-18 11:27:06 +0100

    NOT SUDO :O

--- setup.sh
+++ setup.sh
@@ -3,13 +3,6 @@
 # Check if sudo installed 
 
 # Check if not root
-if ! [ $(whoami) == "root" ]; then
-	printf "sudo is required to run. Do the following:\n$ su \n$ apt install sudo\n"
-	user="frikky"
-	echo "$ $user ALL=(ALL:ALL) ALL >> /etc/sudoers"
-	exit
-fi
-
 echo "Installing necessary tools"
 sudo apt install i3 suckless-tools vim-nox sudo chromium python-pip
 
@@ -17,13 +10,13 @@ printf "\nVerifying vmware"
 verifyvmware=$(sudo dmidecode | grep -i vmware) 
 
 if ! [ -z $verifyvmware ]; then
-	apt install open-vm-tools-desktop
+	sudo apt install open-vm-tools-desktop
 else
 	echo "vmware not present - not installing vmware tools" 
 fi
 
 echo "Setting up i3 and vim environment."
-sudo python setup.py
+python setup.py
 
 echo "Setting up pathogen for vim."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
