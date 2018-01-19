0fe7db97b61607b91694a2fb2ab6478d5a712b3b
Fredrik Ødegårdstuen <fodeg@nets.eu>
2018-01-19 07:46:40 -0500

    Added docker install

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 
 # Check if not root
 echo "Installing necessary tools"
-sudo apt install -y i3 suckless-tools vim-nox sudo chromium python-pip net-tools
+sudo apt update -y
+sudo apt install -y i3 suckless-tools vim-nox sudo chromium python-pip net-tools apt-transport-https ca-certificates curl software-properties-common
 
 printf "\nVerifying vmware"
 verifyvmware=$(sudo dmidecode | grep -i vmware) 
@@ -44,4 +45,16 @@ cd ~/.vim/bundle/ && git clone --recursive https://github.com/davidhalter/jedi-v
 echo "Setting up powershell highlights for .ps1 files"
 cd ~/.vim/bundle && git clone https://github.com/PProvost/vim-ps1.git
 
+echo "Setting up docker"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+echo "Downloading docker-compose version 1.18.0 - Might be deprecated"
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Everything should be set up!"
 printf "\n[!!!] Only some more steps. See README"
