c2aea6bad6988f79791e8b007ef566994195a345
frikky <frikky@localhost.localdomain>
2023-06-23 13:39:02 -1100

    Updated setup to work for 2023 things :)

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # Check if not root
 echo "Installing necessary tools"
 sudo apt update -y
-sudo apt install -y i3 suckless-tools vim-nox sudo chromium python-pip net-tools apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y i3 suckless-tools vim-nox sudo chromium python3-pip net-tools apt-transport-https ca-certificates curl software-properties-common
 
 printf "\nVerifying vmware"
 verifyvmware=$(sudo dmidecode | grep -i vmware) 
@@ -16,9 +16,9 @@ else
 fi
 
 printf "\nSetting up golang"
-wget https://storage.googleapis.com/golang/go1.13.3.linux-amd64.tar.gz
-sudo tar -xvf go1.13.3.linux-amd64.tar.gz
-rm go1.13.3.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.20.5.linux-amd64.tar.gz
+sudo tar -xvf go1.20.5.linux-amd64.tar.gz
+rm go1.20.5.linux-amd64.tar.gz
 sudo mv go /usr/local
 sudo ln -s /usr/local/go/bin/go /usr/bin/go
 
@@ -55,27 +55,27 @@ sudo apt-get update
 sudo apt-get install -y docker-ce
 
 echo "Downloading docker-compose version 1.18.0 - Might be deprecated"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.19.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Installing node and npm"
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install build-essential nodejs
 
-echo "Installing python3.7"
+echo "Installing python3.10"
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
-curl -O https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
-tar -xf Python-3.7.3.tar.xz
-cd Python-3.7.3
+curl -O https://www.python.org/ftp/python/3.10.3/Python-3.10.3.tar.xz
+tar -xf Python-3.10.3.tar.xz
+cd Python-3.10.3
 ./configure --enable-optimizations
 make -j 8
 sudo make altinstall
 cd ..
 
 echo "Fixing npm stuff"
-wget https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-x64.tar.xz
-tar xvf node-v10.16.0-linux-x64
-sudo mv node-v10.16.0-linux-x64 /usr/local/node
+wget https://nodejs.org/dist/v20.3.1/node-v20.3.1-linux-x64.tar.xz
+tar xvf node-v20.3.1-linux-x64
+sudo mv node-v20.3.1-linux-x64 /usr/local/node
 sudo ln -s /usr/local/node/bin/* /usr/bin/
 
 # Hello "hidden" directory :^)
